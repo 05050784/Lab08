26a74deb062b0015491074157db9a97d10a4a537
05050784 <37134163+05050784@users.noreply.github.com>
2018-05-27 15:16:51 +0800

    Add files via upload

---
+++ CHECK_POINT_1.ino
@@ -0,0 +1,29 @@
+#include <Ultrasonic.h>
+#include <LiquidCrystal.h> // include the library code
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+#define TRIGGER_PIN 16
+#define ECHO_PIN 17
+//const byte intPin=2;
+//const byte ledPin=13;
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+void setup() 
+{
+  
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+}
+void loop()
+{
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM); // 計算距離，單位: 公分
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN); // 計算距離，單位: 英吋
+  
+  lcd.print("MS: ");
+  lcd.print(microsec);
+  lcd.print(", CM: "); 
+  lcd.print(cmMsec);
+  lcd.print(", IN: ");
+  lcd.print(inMsec);
+  delay(1000);
+}
